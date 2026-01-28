905e59ddb158eba45b4a7672a10517fbd24137e3
Leo Qi <leo@leoqi.ca>
2026-01-28 12:34:30 -0500

    Explicit definition of what packages provided

--- default.nix
+++ default.nix
@@ -1,3 +1,5 @@
+# Run with nix-build -E 'with import <nixpkgs> {}; callPackage ./default.nix {}'
+
 {
   lib,
   python3Packages,
@@ -12,16 +14,38 @@ python3Packages.buildPythonApplication rec {
   # see https://nixos.org/manual/nixpkgs/stable/#buildpythonapplication-function
   pyproject = true;
 
+  # Must manually specify this until hdlparse is in nixpkgs
+  # (or pass in complicated build options)
+  hdlparse = python3Packages.buildPythonApplication rec {
+    pname = "hdlparse";
+    version = "1.0.5";
+
+    # even if project only has setup.py, as this one does.
+    # see https://nixos.org/manual/nixpkgs/stable/#buildpythonapplication-function
+    pyproject = true;
+
+    src = fetchFromGitHub {
+      owner = "leozqi";
+      repo = "hdlparse";
+      rev = "1.0.5";
+      hash = "sha256-QyBZ7wrApX6KhyPGKaxut+x/++K+l6rvrJwUHmb29Dk=";
+    };
+
+    build-system = with python3Packages; [ setuptools ];
+
+    dependencies = [];
+  };
+  
   src = fetchFromGitHub {
     owner = "leozqi";
     repo = "symbolator";
     rev = "1.0.2";
-    hash = "";
+    hash = "sha256-T9liGr5ZxCaoYjkmTvz9tAszOMJLnYRpCDhDEMozOFk=";
   };
 
   build-system = with python3Packages; [ setuptools ];
 
-  dependencies = [
+  dependencies = with python3Packages; [
     hdlparse
   ];
 }
